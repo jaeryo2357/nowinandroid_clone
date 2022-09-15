b0999934b00f6b80994dfd946c8875d0ef4bc768
jaeryo2357 <jaeryo2357@naver.com>
2022-09-15 22:43:35 +0900

    ResourceCard: Add bookmark icon

--- app/src/main/java/com/example/nowinandroid_clone/ui/ResourceCard.kt
+++ app/src/main/java/com/example/nowinandroid_clone/ui/ResourceCard.kt
@@ -1,11 +1,44 @@
 package com.example.nowinandroid_clone.ui
 
 import android.content.res.Configuration
+import androidx.compose.material.Icon
+import androidx.compose.material.IconToggleButton
 import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.BookmarkBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.nowinandroid_clone.R
 import com.example.nowinandroid_clone.data.news.NewsResource
 import com.example.nowinandroid_clone.ui.theme.NiaTheme
+
+@Composable
+fun BookmarkButton(
+    isBookmarked: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val clickActionLabel = stringResource(
+        if (isBookmarked) R.string.unbookmark else R.string.bookmark
+    )
+    IconToggleButton(
+        checked = isBookmarked,
+        onCheckedChange = { onClick() },
+        modifier = modifier.semantics {
+            this.onClick(label = clickActionLabel, action = null)
+        }
+    ) {
+        Icon(
+          imageVector = if (isBookmarked) Icons.Filled.Bookmark else Icons.Filled.BookmarkBorder,
+          contentDescription = null
+        )
+    }
+}
 
 @Composable
 fun ResourceAuthors(
@@ -68,6 +101,26 @@ fun ExpandedResourcePreview() {
     NiaTheme {
         Surface {
             ResourceCardExpanded()
+        }
+    }
+}
+
+@Preview("Bookmark Button")
+@Composable
+fun BookmarkButtonPreview() {
+    NiaTheme {
+        Surface {
+            BookmarkButton(isBookmarked = false, onClick = { })
+        }
+    }
+}
+
+@Preview("Bookmark Button Bookmarked")
+@Composable
+fun BookmarkButtonBookmarkedPreview() {
+    NiaTheme {
+        Surface {
+            BookmarkButton(isBookmarked = true, onClick = { })
         }
     }
 }
